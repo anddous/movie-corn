b3ff1aef63af31ce22c9b51ee9eaaf64540392a2
Andrea Dousova <anddous@gmail.com>
2018-11-17 16:47:51 +0100

    prejmenovani

--- DatabaseService_Lukas.py
+++ DatabaseService_Lukas.py
@@ -9,8 +9,8 @@ class DatabaseService:
 		# Create a cursor from the connection
 		self.cursor = self.databaseConnection.cursor()
 
-	def get_movies(self, yearFrom, yearTo, category, runtimeFrom, runtimeTo, ratingFrom, movieType, ratingTo):
+	def get_movies(self, yearFrom, yearTo):
 		#try:
-			return self.cursor.execute("exec dbo.usp_GetMovies ?, ?, ?, ?, ?, ?, ?, ?", yearFrom, yearTo, category, runtimeFrom, runtimeTo, ratingFrom, movieType, ratingTo)
+			return self.cursor.execute("exec dbo.usp_GetMovies ?, ?", yearFrom, yearTo)
 		#except Exception as e:
 		#	print("\n" + "DatabaseService error :" + "\n" + str(e) + "\n")
